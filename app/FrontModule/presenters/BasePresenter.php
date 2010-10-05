b757462bd8e5dd0b3cc2e85f5b96994242876ba9
Igor Hlina <srigi@srigi.sk>
2010-10-05 21:02:37 +0200

    Added texy! to frontend rendering.

--- app/FrontModule/presenters/BasePresenter.php
+++ app/FrontModule/presenters/BasePresenter.php
@@ -3,13 +3,26 @@
 namespace FrontModule;
 
 
+use	Nette\Templates\TemplateFilters,
+	Texy;
+
+
 class BasePresenter extends \BasePresenter
 {
 
 	protected function createTemplate()
 	{
 		$template = parent::createTemplate();
+
+		$texy = new Texy();
+		$texy->encoding = 'utf-8';
+		$texy->allowedTags = Texy::NONE;
+		$texy->allowedStyles = Texy::NONE;
+		$texy->setOutputMode(Texy::HTML5);
+
+		$template->registerHelper('texy', callback($texy, 'process'));
 		$template->registerHelper('perex', 'NBlog\Templates\Helpers::perex');
+
 		return $template;
 	}
 
